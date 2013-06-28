940e63234f0183a7d2fddcbcc71e0f89e416c3c7
Stéphane Couture <steph@stephcouture.info>
2013-06-28 19:18:18 -0400

    Implementing an alert system in the case some lines are not parsed properyly

--- parsers/parser.js
+++ parsers/parser.js
@@ -15,13 +15,29 @@ Parser.prototype.parse_logs = function(i_source, max) {
 	 	return this.logs;
 
 	var lines = i_source.split(/\n+/);
+	no_matches_lines = "";
+	
 	for (var i=0, len=(max<lines.length ? max : lines.length); i<len; i++) {				
 		var parsed_line = this.parse_line(lines[i]);
 		if (parsed_line)
 		   this.logs.push(parsed_line);
-		else
+		else {
+			no_matches_lines += lines[i] + "\n"; 
 			console.warn("parser no matches : " + lines[i]);
+		}
+			
 	}
+	
+	if (no_matches_lines) {
+		var alertstring = "WARNING!!\n\nThe following lines were not parsed correctly. ";
+		alertstring += "If they are signigicant, please verify that you chose the good parser and that your log text is well formatted. ";
+		alertstring += "Othewise, contact us by sending these lines and specifying which parser you are using\n\n";
+		alertstring += no_matches_lines;
+		
+		
+		alert(alertstring);
+	}
+		
 		
 	this.parsed = true;
 
